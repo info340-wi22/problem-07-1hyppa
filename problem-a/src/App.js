4b1905bc15c2b3d3491eb60f3d94aaeaf703995c
gosha <ghyppa@uw.edu>
2022-02-15 23:05:26 -0800

    started problem-a

--- problem-a/src/App.js
+++ problem-a/src/App.js
@@ -1,8 +1,62 @@
 import React from 'react'; //import React library
 
-const EXAMPLE_SENATORS = [  
+const EXAMPLE_SENATORS = [
   { id: 'C000127',  name: 'Maria Cantwell', state: 'WA',  party: 'Democrat', phone: '202-224-3441', twitter: 'SenatorCantwell' },
   { id: 'M001111', name: 'Patty Murray', state: 'WA', party: 'Democrat', phone: '202-224-2621', twitter: 'PattyMurray' }
 ];
 
+const headerArray = ['Name', 'State', 'Phone', 'Twitter'];
+
 /* Your code goes here */
+export function App(props) {
+  return (
+    <div className='container'>
+      <h1>US Senators (Jan 2021)</h1>
+      <SenatorTable />
+    </div>
+  )
+}
+
+export function SenatorTable(props) {
+  // let senatorArr = props.senators.map((element) => {
+  //   return (<SenatorRow key={element.id}/>)
+
+  // })
+  let senatorArr = EXAMPLE_SENATORS.map((element) => {
+    return <SenatorRow key={element.id}/>
+  })
+  console.log(senatorArr);
+  return (
+    <table className='table table-bordered'>
+      <TableHeader columnNames={headerArray} />
+      <tbody>
+        {senatorArr}
+      </tbody>
+    </table>
+  )
+}
+
+export function TableHeader(props) {
+  let thElem = props.columnNames.map((nameString) => {
+    return <th key={nameString}>{nameString}</th>;
+  })
+  return (
+    <thead>
+      <tr>
+        {thElem}
+      </tr>
+    </thead>
+  );
+}
+
+export function SenatorRow(props) {
+  const {id, name, state, party, phone, twitter} = props;
+  return (
+    <tr key={id}>
+      <td>{name}</td>
+      <td>{state}</td>
+      <td>{phone}</td>
+      <td>{twitter}</td>
+    </tr>
+  )
+}
--- problem-a/src/index.js
+++ problem-a/src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {App} from './App.js';
 
 //render the App component here!
+let renderElem = <App />;
+ReactDOM.render(renderElem, document.getElementById("root"));
